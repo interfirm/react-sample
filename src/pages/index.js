8e8832b62b5753eec1dfe977a736708a14a53690
Kazuma Yamazaki <kazuma.yamazaki@interfirm.co.jp>
2019-11-26 10:37:29 +0900

    feat: components の追加 / 修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,16 +16,33 @@ export default () => {
   );
   const handleChangeHeadline = event => setHeadline(event.target.value);
 
+  const [numbers, setNumbers] = useState([1, 2, 3])
+  const handleChangeNumbers = num => setNumbers(
+    [numbers[0], num, numbers[2]]
+  );
+  const handleInputNumbers = event => setNumbers([
+    Number(event.target.value), Number(event.target.value), Number(event.target.value)]
+  );
+
   return (
     <div>
       <h1>Index page</h1>
+
       <h2>Counter</h2>
       <Counter initialCount={0} />
+
       <h2>Counter 2</h2>
       <Counter2 counter={counter} onChangeCounter={handleChangeCounter} inputCounter={handleInputCounter} />
 
       <Headline headline={headline} onChangeHeadline={handleChangeHeadline} />
       <Headline2 initialHeadline={"test"} />
+
+      <h2>Numbers: {numbers.join(", ")}</h2>
+      <h2>Numbers: {numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;}, 0)}
+      </h2>
+
+      <Counter2 counter={numbers[1]} onChangeCounter={handleChangeNumbers} inputCounter={handleInputNumbers} />
     </div>
   );
 };
